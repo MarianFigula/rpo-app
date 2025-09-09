40a6f079df5030aa8af7e1dfb8a22ff3df4c9083
Marian Figula <majofigulam3@gmail.com>
2025-09-09 17:21:37 +0200

    feat: removed unnecessary code from header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,20 +1,7 @@
-import {useEffect, useState} from "react";
-
-
 const Header = () => {
-    const [isScrolled, setIsScrolled] = useState(false);
-    useEffect(() => {
-        const handleScroll = () => {
-            setIsScrolled(window.scrollY > 50);
-        };
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
 
     return (
-        //${ isScrolled ? 'bg-background/95 backdrop-blur-sm shadow-sm border-border/50' : 'bg-transparent'
-        //         }
-        <header className={`fixed top-0 w-full z-50 transition-all duration-300 bg-indigo-900`}>
+        <header className={`z-50 transition-all duration-300 bg-indigo-900 mb-15`}>
             <nav className="px-8 py-8">
                 <div className="flex items-center justify-center">
                     <div className="text-3xl font-bold text-white">
