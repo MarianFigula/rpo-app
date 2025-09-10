37808e1490ddac703df0c13933658a39f52ad5d8
Marian Figula <majofigulam3@gmail.com>
2025-09-10 18:38:26 +0200

    feat: created get-advertisements api

---
+++ backend/api/advertisement/get-advertisements.php
@@ -0,0 +1,44 @@
+<?php
+
+include_once '../../config/cors.php';
+include_once '../../config/Database.php';
+include_once '../../Advertisement/Repository/AdvertisementRepository.php';
+
+use Advertisement\Repository\AdvertisementRepository;
+use config\Database;
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method !== "GET") {
+    http_response_code(405);
+    echo json_encode([
+        "success" => false,
+        "message" => "Method not allowed. Only POST is permitted."
+    ]);
+    exit();
+}
+
+try {
+
+    $database = new Database();
+    $advertisementRepository = new AdvertisementRepository($database);
+
+    $advertisements = $advertisementRepository->getAdvertisements();
+
+    http_response_code(200);
+    echo json_encode([
+        "success" => true,
+        "message" => "Advertisements retrieved successfully",
+        "data" => [
+            "advertisements" => $advertisements
+        ]
+    ]);
+
+}catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode([
+        "success" => false,
+        "message" => "An error occurred: " . $e->getMessage()
+    ]);
+}
