adf859994cc7183085f29af6b94b582d411d8828
Marian Figula <majofigulam3@gmail.com>
2025-09-10 17:47:45 +0200

    feat: created AdvertisementRepository.php

---
+++ backend/Advertisement/Repository/AdvertisementRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Advertisement\Repository;
+
+use config\Database;
+use Exception;
+use PDO;
+use PDOException;
+
+class AdvertisementRepository
+{
+    private ?PDO $conn;
+
+    public function __construct(Database $database)
+    {
+        $this->conn = $database->getConnection();
+    }
+
+    public function addAdvertisement($data)
+    {
+        if (empty($data)){
+            return false;
+        }
+
+        $id = $data['company_id'] ?? null;
+        $text = $data['text'] ?? null;
+
+        if (!$id || !$text) {
+            return false;
+        }
+
+        $query = "INSERT INTO advertisement (company_id, text) VALUES(?,?)";
+
+        try {
+            $stmt = $this->conn->prepare($query);
+            return $stmt->execute([
+                $id,
+                $data['text'],
+            ]);
+
+        }catch (PDOException $e){
+            throw new Exception("Database query failed: " . $e->getMessage());
+        }
+
+    }
+
+}
