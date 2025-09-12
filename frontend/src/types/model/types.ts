a01c6ac2e36b441144378cae0a1c5179dfbe528f
Marian Figula <majofigulam3@gmail.com>
2025-09-12 08:19:10 +0200

    chores: remove unused types

--- frontend/src/types/model/types.ts
+++ frontend/src/types/model/types.ts
@@ -16,16 +16,6 @@ export interface Company {
 
 export type CompanyCardModel = Omit<Company, 'score' | 'created_at' | 'updated_at'>
 
-export type CompanySearch = Omit<Company, 'logo_url' | 'created_at' | 'updated_at'>
-
-export interface Advertisement {
-    id: string,
-    company_id: string,
-    text: string;
-    created_at: string,
-    updated_at: string
-}
-
 
 export interface AdvertisementCardModel {
     id: string,
